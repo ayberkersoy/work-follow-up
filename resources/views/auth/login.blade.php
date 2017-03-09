a0a843c62b12b2fb243d64e74bf129b5d7933b6d
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-03-09 14:25:35 +0300

    logout route and login blade fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,11 +45,11 @@
         <form action="login" method="post">
             {{ csrf_field() }}
             <div class="form-group has-feedback">
-                <input type="text" class="form-control" placeholder="Kullanıcı Adı" name="username">
+                <input type="text" class="form-control" placeholder="Kullanıcı Adı" name="username" autocomplete="off">
                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <input type="password" class="form-control" placeholder="Şifre" name="password">
+                <input type="password" class="form-control" placeholder="Şifre" name="password" autocomplete="off">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
