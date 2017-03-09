12e1b760e72fcda582439a5d7bc831b6ba93b108
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-03-09 11:24:28 +0300

    projects table

---
+++ database/migrations/2017_03_09_082148_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firm_name');
+            $table->string('project_name');
+            $table->string('address');
+            $table->string('authorized');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
