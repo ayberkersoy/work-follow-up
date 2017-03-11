ec2182f17c906247bcd25bdb0d9403dce062bcbc
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-03-11 22:56:57 +0300

    tables

---
+++ database/migrations/2017_03_11_151047_create_discoveries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscoveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discoveries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('dis_category_id');
+            $table->integer('project_id');
+            $table->string('job');
+            $table->string('description');
+            $table->string('amount');
+            $table->string('unit');
+            $table->string('unit_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discoveries');
+    }
+}
