36e31092c72e11c19412cc3094b06d615bee0dfb
Ayberk Ersoy <ayberkersoy@gmail.com>
2017-03-24 12:15:04 +0300

    index by user

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -70,56 +70,58 @@
                     <section class="col-lg-12 connectedSortable" style="background-color: #fff">
                         <div class="col-sm-12">
                             @if(Auth()->user()->index == 1)
-
-                            @endif
-                            <h4>Son Projeler</h4>
-                            <table class="table table-hover table-bordered table-striped">
-                                <tr>
-                                    <th>Proje Adı</th>
-                                    <th>Firma Adı</th>
-                                    <th>Yetkili Kişi</th>
-                                    <th>Başlama Tarihi</th>
-                                    <th>Bitiş Tarihi</th>
-                                    <th>Görüntüle</th>
-                                </tr>
-                                <tr>
-                                    @foreach($projects as $project)
-                                        <td>{{ $project->project_name }}</td>
-                                        <td>{{ $project->firm_name }}</td>
-                                        <td>{{ $project->authorized }}</td>
-                                        <td>{{ $project->start_date }}</td>
-                                        <td>{{ $project->end_date }}</td>
-                                        <td>
-                                            <a href="/proje-detay/{{ $project->id }}"><i class="fa fa-eye"></i></a>
-                                        </td>
+                                <h4>Son Notlar</h4>
+                                <table class="table table-hover table-bordered table-striped">
+                                    <tr>
+                                        <th>Atayan Kişi</th>
+                                        <th>Atanan Kişi</th>
+                                        <th>Not Kategorisi</th>
+                                        <th>Not İçeriği</th>
+                                        <th>Görüntüle</th>
+                                    </tr>
+                                    @foreach($notes as $note)
+                                        @if($note->note->status == 0)
+                                            <tr>
+                                                <td>{{ $note->fromUser->username }}</td>
+                                                <td>{{ $note->user->username }}</td>
+                                                <td>{{ $note->note->category->name }}</td>
+                                                <td>{{ substr($note->note->content, 0, 100) }}</td>
+                                                <td>
+                                                    <a href="/not/{{ $note->id }}"><i class="fa fa-eye"></i></a>
+                                                </td>
+                                            </tr>
+                                        @endif
                                     @endforeach
-                                </tr>
-                            </table>
+                                </table>
+
+                            @elseif(Auth()->user()->index == 2)
+                                <h4>Son Projeler</h4>
+                                <table class="table table-hover table-bordered table-striped">
+                                    <tr>
+                                        <th>Proje Adı</th>
+                                        <th>Firma Adı</th>
+                                        <th>Yetkili Kişi</th>
+                                        <th>Başlama Tarihi</th>
+                                        <th>Bitiş Tarihi</th>
+                                        <th>Görüntüle</th>
+                                    </tr>
+                                    <tr>
+                                        @foreach($projects as $project)
+                                            <td>{{ $project->project_name }}</td>
+                                            <td>{{ $project->firm_name }}</td>
+                                            <td>{{ $project->authorized }}</td>
+                                            <td>{{ $project->start_date }}</td>
+                                            <td>{{ $project->end_date }}</td>
+                                            <td>
+                                                <a href="/proje-detay/{{ $project->id }}"><i class="fa fa-eye"></i></a>
+                                            </td>
+                                        @endforeach
+                                    </tr>
+                                </table>
+                            @endif
                         </div>
                         <div class="col-sm-12">
-                            <h4>Son Notlar</h4>
-                            <table class="table table-hover table-bordered table-striped">
-                                <tr>
-                                    <th>Atayan Kişi</th>
-                                    <th>Atanan Kişi</th>
-                                    <th>Not Kategorisi</th>
-                                    <th>Not İçeriği</th>
-                                    <th>Görüntüle</th>
-                                </tr>
-                                @foreach($notes as $note)
-                                    @if($note->note->status == 0)
-                                        <tr>
-                                            <td>{{ $note->fromUser->username }}</td>
-                                            <td>{{ $note->user->username }}</td>
-                                            <td>{{ $note->note->category->name }}</td>
-                                            <td>{{ substr($note->note->content, 0, 100) }}</td>
-                                            <td>
-                                                <a href="/not/{{ $note->id }}"><i class="fa fa-eye"></i></a>
-                                            </td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                            </table>
+
                         </div>
                     </section><!-- /.Left col -->
                 </div><!-- /.row (main row) -->
